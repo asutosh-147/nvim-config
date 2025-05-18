b7fd6e77acdcd91e7aed57db319e30f07261e328
asutosh-147 <behuriaasutosh7@gmail.com>
2025-05-19 02:27:43 +0530

    FIX:clipboard sync issue

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,10 +6,10 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "move highlighted line up"
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "source current file" })
--- TODO: Handle System clipboard and nvim registers differently
-vim.keymap.set("x", "<leader>P", '"_dP', { desc = "Paste over selection without erasing unnamed register" })
+
+-- copy and paste from system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "copy to system clipboard" })
+vim.keymap.set("n", "<leader>Y", '"+Y', { desc = "copy line to system clipboard" })
 vim.keymap.set({ "n", "v", "x" }, "<leader>p", '"+p', { noremap = true, silent = true, desc = "Paste from clipboard" })
-vim.keymap.set("n", "<leader>y", '"+y', { desc = "copy to system clipboard" })
-vim.keymap.set("v", "<leader>y", '"+y', { desc = "copy to system clipboard" })
-vim.keymap.set("n", "<leader>k", '"_d', { desc = "delete not copying to clipboard" })
-vim.keymap.set("v", "<leader>k", '"_d', { desc = "delete not copying to clipboard" })
+
+vim.keymap.set("x", "<leader>P", '"_dP', { desc = "Paste over selection without erasing unnamed register" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,6 +3,11 @@
 -- Add any additional options here
 vim.opt.scrolloff = 8
 vim.opt.incsearch = true
+
+-- removing sync with system clipboard always
+vim.opt.clipboard = "" -- default: unnamedplus
+
+-- add support for clipboard sync with +,* registers
 vim.g.clipboard = {
   name = "win32yank-wsl",
   copy = {
