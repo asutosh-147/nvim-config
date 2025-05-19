83afffa7a56b0527cfd6c0765e56d60caa236609
Asutosh Behuria <behuriaasutosh7@gmail.com>
2025-05-19 15:06:55 +0530

    formatter setup

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+  'stevearc/conform.nvim',
+  opts={
+    formatters_by_ft = {
+      javascript = {"prettier"},
+      typescript = {"prettier","prettierd","biome", stop_after_first = true}
+    },
+    formatters={
+      prettier = {
+        require_cwd = false,
+      },
+    }
+  },
+}
