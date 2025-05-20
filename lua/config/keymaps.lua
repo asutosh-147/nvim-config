12de95e8fa0ebd7bda4ef539384da5ab4befa243
Asutosh Behuria <behuriaasutosh7@gmail.com>
2025-05-20 16:37:06 +0530

    UPD:keymap update to start i with auto-indent

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,3 +13,8 @@ vim.keymap.set("n", "<leader>Y", '"+Y', { desc = "copy line to system clipboard"
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { noremap = true, silent = true, desc = "Paste from clipboard" })
 
 vim.keymap.set("x", "<leader>P", '"_dP', { desc = "Paste over selection without erasing unnamed register" })
+vim.keymap.set("n", "<leader>(", "viw<esc>a)<esc>hbi(<esc>", { desc = "add () surrounding curr word" })
+-- make insert mode start with auto indent
+vim.keymap.set("n", "i", function()
+  return string.match(vim.api.nvim_get_current_line(), "%g") == nil and "cc" or "i"
+end, { expr = true, noremap = true })
