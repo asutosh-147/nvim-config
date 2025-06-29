6bf27b58e36997b5a0205a9d042cd3a589cb69f5
asutosh-147 <behuriaasutosh7@gmail.com>
2025-06-29 13:09:19 +0530

    UPD:added mason required list

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,18 @@
+return {
+  "mason-org/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shfmt",
+      "gopls",
+      "goimports",
+      "lua-language-server",
+      "vtsls",
+      "prettierd",
+      "delve",
+      "tailwindcss-language-server",
+      "js-debug-adapter",
+      "eslint-lsp",
+    },
+  },
+}
