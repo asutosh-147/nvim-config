580c646627d6aadb87622685b47b341a862c11e7
Asutosh Behuria <behuriaasutosh7@gmail.com>
2026-03-19 19:39:00 +0530

    UPD: added some games

---
+++ lua/plugins/games.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "jim-fx/sudoku.nvim",
+    cmd = "Sudoku",
+    config = function()
+      require("sudoku").setup({
+        -- configuration ...
+      })
+    end,
+  },
+  {
+    "alec-gibson/nvim-tetris",
+  },
+}
